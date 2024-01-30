7a3cd0d2b02e242f24edcc46385a5944f7f856bd
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-30 13:22:16 +0100

    add constructor to class movie

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
         public $title;
         public $genre;
         public $year;
+
+        function __construct($_title, $_genre, $_year){
+            $this->name = $name;
+            $this->genre = $genre;
+            $this->year = $year;
+        }
     }
+
+
 ?>
